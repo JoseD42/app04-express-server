6fbdc783cdaa49a6f661d80bc91b72edfd9a373b
JoseD42 <josedanielbecerra42@gmail.com>
2020-09-07 00:22:00 -0600

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 const port = 5000;
 
-app.use('/', (request, response) =>{
-    response.send('<h1>Hello my custom server</h1>');
+app.use(bodyParser.json());//cosumir cosas
+
+//Metodo GET o Obtener
+app.get('/', (request, response) =>{
+    response.send({name: 'José Daniel', age: 19});
+});
+
+app.post('/', (request, response) => {
+    const {price, quatity} = request.body;
+    response.send({tag: `Llevate ${quatity} unidades por tan solo ${price} pesos`});
 })
 
 app.listen(port, ()=> console.log(`Saver started at: http://localhost:${port}`));
